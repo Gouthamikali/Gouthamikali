9bf9cb7d4ee5198307b3eeb1609cecfcf5a79ed3
Gouthamikali <141644725+Gouthamikali@users.noreply.github.com>
2024-02-18 10:55:42 +0530

    Create vowel consonants.java

---
+++ vowel consonants.java
@@ -0,0 +1,30 @@
+BEGIN 
+  SET vowelC to 'a'
+  SWITCH(vowelC)THEN
+  START SWITCH
+      CASE "A"
+      CASE 'a'
+           PRINT vowelC"VOWEL"
+           BREAK
+     CASE "E"
+     CASE 'e'
+           PRINT vowelC+"VOWEL"
+           BREAK
+      CASE "I"
+      CASE 'i'
+           PRINT"VOWEL"
+           BREAK
+      CASE "O"
+      CASE 'o'
+           PRINT"VOWEL"
+           BREAK
+      CASE "U"
+      CASE 'u'
+           PRINT"VOWEL"
+           BREAK
+      DEFAULT 
+         PRINT"GIVEN ALPHATBET IS CONSONAT"
+   END SWITCH
+END
+                 
+ 
