9e0729aa842f19f3351d16dbb6be52b70e520906
Gouthamikali <141644725+Gouthamikali@users.noreply.github.com>
2024-02-18 10:51:38 +0530

    Create vote eligible.java

---
+++ vote eligible.java
@@ -0,0 +1,11 @@
+BEGIN 
+   SET age to 43
+   IF age is graeter than 21 THEN
+   START IF
+      PRINT"given age is eligible foe IAS"
+   END IF
+   ELSE
+   START ELSE
+       PRINT"given age is elible foe IAS"
+  END ELSE
+END
